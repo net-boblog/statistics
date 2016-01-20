e2da9843c050bdacbfd061dc40d1a24619d71966
caedmon <caedmon@52xiaoluo.com>
2016-01-20 15:08:54 +0800

    修改查询条件

--- src/main/webapp/resources/js/list.js
+++ src/main/webapp/resources/js/list.js
@@ -45,7 +45,7 @@ function saveDict($tr) {
     if ( !data.type || !data.description ){
         alert('请选择字段类型并填写字段描述');
     }else{
-        $.get(ROOT + '/dict/update',data,function(data){
+        $.post(ROOT + '/dict/update',data,function(data){
             console.debug(data);
             if (data.code == 0) {
                 getDictList();
